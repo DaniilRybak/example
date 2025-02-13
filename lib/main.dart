36421b9792a06143dfee0369e43c689d038f3f69
DaniilRybak <daniil_rus161@mail.ru>
2025-02-13 18:36:15 +0300

    v0.0.1

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:yandex_mapkit/yandex_mapkit.dart';
 
 class MarkerData {
   final String id;
   final double latitude;
   final double longitude;
+  final String type; // full, empty, route
 
   MarkerData({
     required this.id,
     required this.latitude,
     required this.longitude,
+    required this.type,
   });
 }
 
-// Функция, возвращающая тестовые данные для меток
+// Функция, возвращающая тестовые данные для меток с разными типами
 Future<List<MarkerData>> fetchTestMarkers() async {
   return [
-    MarkerData(id: 'placemark_1', latitude: 55.756, longitude: 37.618),
-    MarkerData(id: 'placemark_2', latitude: 59.956, longitude: 30.3139),
-    MarkerData(id: 'placemark_3', latitude: 59.956, longitude: 30.313),
+    MarkerData(id: 'placemark_1', latitude: 55.756, longitude: 37.618, type: 'full'),
+    MarkerData(id: 'placemark_2', latitude: 59.956, longitude: 30.313, type: 'empty'),
+    MarkerData(id: 'placemark_3', latitude: 59.956, longitude: 30.3135, type: 'route'),
   ];
 }
 
-/// Виджет карты с установкой начальных координат камеры
 class MyMapPage extends StatefulWidget {
   final Point initialCameraTarget;
   const MyMapPage({super.key, required this.initialCameraTarget});
@@ -43,54 +45,85 @@ class _MyMapPageState extends State<MyMapPage> {
     loadMarkers();
   }
 
-  Future<void> loadMarkers() async {
-    try {
-      final markers = await fetchTestMarkers();
-      final placemarks = markers.map((marker) {
-        return PlacemarkMapObject(
-          mapId: MapObjectId(marker.id),
-          point: Point(latitude: marker.latitude, longitude: marker.longitude),
-          icon: PlacemarkIcon.single(
-            PlacemarkIconStyle(
-              image: BitmapDescriptor.fromAssetImage('lib/assets/place.png'),
-              scale: 1,
+Future<void> loadMarkers() async {
+  try {
+    final markers = await fetchTestMarkers();
+    final placemarks = markers.map((marker) {
+      // Выбор изображения в зависимости от типа метки:
+      String asset;
+      switch (marker.type) {
+        case 'full':
+          asset = 'lib/assets/route_start.png';
+          break;
+        case 'empty':
+          asset = 'lib/assets/route_end.png';
+          break;
+        case 'route':
+          asset = 'lib/assets/route_stop_by.png';
+          break;
+        default:
+          asset = 'lib/assets/place.png';
+          break;
+      }
+      return PlacemarkMapObject(
+        mapId: MapObjectId(marker.id),
+        point: Point(latitude: marker.latitude, longitude: marker.longitude),
+        icon: PlacemarkIcon.single(
+          PlacemarkIconStyle(
+            image: BitmapDescriptor.fromAssetImage(asset),
+            scale: 1,
+          ),
+        ),
+      );
+    }).toList();
+
+    // Добавляем метку с текущей геолокацией
+    final Position position = await _determinePosition();
+    final currentLocationPlacemark = PlacemarkMapObject(
+      mapId: const MapObjectId('current_location'),
+      point: Point(latitude: position.latitude, longitude: position.longitude),
+      icon: PlacemarkIcon.single(
+        PlacemarkIconStyle(
+          image: BitmapDescriptor.fromAssetImage('lib/assets/user.png'),
+          scale: 1,
+        ),
+      ),
+    );
+
+    placemarks.add(currentLocationPlacemark);
+
+    final clusterized = ClusterizedPlacemarkCollection(
+      mapId: clusterId,
+      radius: 30,
+      minZoom: 15,
+      placemarks: placemarks,
+      onClusterAdded: (ClusterizedPlacemarkCollection self, Cluster cluster) async {
+        return cluster.copyWith(
+          appearance: cluster.appearance.copyWith(
+            icon: PlacemarkIcon.single(
+              PlacemarkIconStyle(
+                image: BitmapDescriptor.fromAssetImage('lib/assets/cluster.png'),
+                scale: 1,
+              ),
             ),
           ),
         );
-      }).toList();
+      },
+      onClusterTap: (ClusterizedPlacemarkCollection self, Cluster cluster) {
+        print('Tapped cluster');
+      },
+      onTap: (ClusterizedPlacemarkCollection self, Point point) {
+        print('Tapped at $point');
+      },
+    );
 
-      final clusterized = ClusterizedPlacemarkCollection(
-        mapId: clusterId,
-        radius: 30,
-        minZoom: 15,
-        placemarks: placemarks,
-        onClusterAdded: (ClusterizedPlacemarkCollection self, Cluster cluster) async {
-          return cluster.copyWith(
-            appearance: cluster.appearance.copyWith(
-              icon: PlacemarkIcon.single(
-                PlacemarkIconStyle(
-                  image: BitmapDescriptor.fromAssetImage('lib/assets/cluster.png'),
-                  scale: 1,
-                ),
-              ),
-            ),
-          );
-        },
-        onClusterTap: (ClusterizedPlacemarkCollection self, Cluster cluster) {
-          print('Tapped cluster');
-        },
-        onTap: (ClusterizedPlacemarkCollection self, Point point) {
-          print('Tapped at $point');
-        },
-      );
-
-      setState(() {
-        mapObjects.add(clusterized);
-      });
-    } catch (error) {
-      print("Ошибка при загрузке меток: $error");
-    }
+    setState(() {
+      mapObjects.add(clusterized);
+    });
+  } catch (error) {
+    print("Ошибка при загрузке меток: $error");
   }
+}
 
   @override
   Widget build(BuildContext context) {
@@ -220,4 +253,31 @@ class MyApp extends StatelessWidget {
       home: MainScreen(),
     );
   }
+}
+
+Future<Position> _determinePosition() async {
+  bool serviceEnabled;
+  LocationPermission permission;
+
+  // Проверяем, включены ли службы геолокации
+  serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  if (!serviceEnabled) {
+    return Future.error('Службы геолокации отключены.');
+  }
+
+  // Проверяем разрешения на доступ к геолокации
+  permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      return Future.error('Разрешения на доступ к геолокации отклонены.');
+    }
+  }
+
+  if (permission == LocationPermission.deniedForever) {
+    return Future.error('Разрешения на доступ к геолокации отклонены навсегда.');
+  }
+
+  // Получаем текущую позицию
+  return await Geolocator.getCurrentPosition();
 }
